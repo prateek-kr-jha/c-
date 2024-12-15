b7371f46ec7007109334bdc8738e9275b1450c33
prateek <prateek.jha@yulu.bike>
2024-12-16 01:24:36 +0530

    the cherno

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 int main() {
     int x { 14 };
     auto result = (10 <=> 20) > 0;
-    std::cout << result << " hi there " << x << std::endl;
+    std::cout << result << " hi there "  << std::endl;
+    std::cin.get();
+    std::cin >> x;
+    std::cout << x << "\n";
 }
