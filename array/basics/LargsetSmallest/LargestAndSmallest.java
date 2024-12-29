44aea1b05c8f97de2555a141f8edf3c1ebd702dc
Prateek <prateekj1211@gmail.com>
2024-12-29 18:20:12 +0530

    largest and smallest in an array

---
+++ array/basics/LargsetSmallest/LargestAndSmallest.java
@@ -0,0 +1,58 @@
+package array.basics.LargsetSmallest;
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class LargestAndSmallest {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void fillArray(int[] arr) {
+        int n = arr.length;
+        int i = 0;
+        while(i < n && scn.hasNext()) {
+            arr[i] = scn.nextInt();
+            i++;
+        }
+
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for(int i :  arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static HashMap<String, Integer> largestSmallest(int[] arr) {
+        HashMap<String, Integer> sol = new HashMap<String, Integer>();
+        int smallest = arr[0];
+        int largest = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > largest) {
+                largest = arr[i];
+            }
+
+            if(arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+        sol.put("Largest", largest);
+        sol.put("Smallest", smallest);
+
+        return sol;
+        
+    }
+
+    public static void main(String[] args) {
+
+        int size = scn.nextInt();
+        int[] inputArray = new int[size];
+        fillArray(inputArray);
+        printArray(inputArray);
+        HashMap<String, Integer> ans = largestSmallest(inputArray);
+        System.out.println(ans);
+
+    }
+}
