58ff3927bef4a6cdce76229e388899513bc93ec6
Prateek <prateekj1211@gmail.com>
2025-01-06 21:57:14 +0530

    implemented merge sort with in place merge

--- array/basics/removeDuplicate/removeDuplicate.java
+++ array/basics/removeDuplicate/removeDuplicate.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class removeDuplicate {
     public static Scanner scn = new Scanner(System.in);
+    private static int[] aux;
 
     public static void fillArray(int[] arr) {
         int n = arr.length;
@@ -22,12 +23,20 @@ public class removeDuplicate {
         System.out.println();
     }
 
-    // public static void mergeSort(int[] arr, int i, int j) {
-    //     if(i = j) {
-    //         return;
-    //     }
-    // }
-    public static void merge(int[] arr, int[] aux, int lo, int mid, int hi) {
+    public static void mergeSort(int[] arr) {
+        aux = new int[arr.length];
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int lo, int hi) {
+        if(hi <= lo) return;
+        int mid = lo + (hi - lo)/2;
+        mergeSort(arr, lo, mid);
+        mergeSort(arr,  mid + 1, hi);
+        merge(arr, lo, mid, hi);
+    }
+
+    public static void merge(int[] arr, int lo, int mid, int hi) {
         int i = lo, j = mid + 1;
 
         for(int k = lo; k <= hi; k++) {
@@ -35,7 +44,6 @@ public class removeDuplicate {
         }
 
         for(int k = lo; k <= hi; k++) {
-            System.out.println(k + " : i " + i + " : j " + j);
             if(i > mid) {
                 arr[k] = aux[j++];
             } else if(j > hi) {
@@ -46,19 +54,17 @@ public class removeDuplicate {
                 arr[k] = aux[i++];
             } 
         }
-        printArray(arr);
     }
 
     public static void main(String[] args) {
 
         int size = scn.nextInt();
         int[] inputArray = new int[size];
-        int[] aux = new int[size];
-        int mid = size/2;
+
         // System.out.println(mi);
         fillArray(inputArray);
         printArray(inputArray);
-        merge(inputArray, aux, 0, mid, size - 1);
+        mergeSort(inputArray);
         printArray(inputArray);
 
     }
